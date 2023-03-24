2f6f2e4f4ffbcad639f097163129e2ce148f41e2
Laoise Earle <laoiseearle92@gmail.com>
2023-03-24 17:05:24 +0000

    Add function to close menu at window resize

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const ham = document.getElementById('ham');
 const menu = document.getElementById('menu');
 
+window.addEventListener('resize', e => {
+  if (e.target.innerWidth >= 768) {
+    menu.classList.add('menu-open');
+    ham.style.filter = 'opacity(.25)';
+  }
+});
+
 const openMenu = () => {
   if (menu.classList.contains('menu-open')) {
     menu.classList.remove('menu-open');
