b90383759e00c9c3456711cabe2ac7922f174bd1
Laoise Earle <laoiseearle92@gmail.com>
2023-12-08 10:05:06 +0000

    Hide main content when menu is open

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const ham = document.getElementById('ham');
 const menu = document.getElementById('menu');
+const mainContent = document.querySelector('main');
 
 window.addEventListener('resize', e => {
   if (e.target.innerWidth >= 768) {
     menu.classList.remove('menu-open');
     ham.style.filter = 'opacity(1)';
+    mainContent.style.display = 'block';
   }
 });
 
@@ -12,9 +14,11 @@ const openMenu = () => {
   if (menu.classList.contains('menu-open')) {
     menu.classList.remove('menu-open');
     ham.style.filter = 'opacity(1)';
+    mainContent.style.display = 'block';
   } else {
     menu.classList.add('menu-open');
     ham.style.filter = 'opacity(.25)';
+    mainContent.style.display = 'none';
   }
 };
 
